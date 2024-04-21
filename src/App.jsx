6d3bbf7c82f17bc3a508ee452b9189b5362d053a
Bobby Steckline <rsteckline@gmail.com>
2024-04-21 11:29:32 -0400

    Add HTML structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-xl font-bold">Hello world</h1>
-    </>
+    <main className="w-full h-screen flex justify-center items-center bg-dark-blue">
+      <div className="flex flex-col gap-8 max-w-[33.7rem] m-[1.3rem] mb-[4rem]">
+        <div className="w-full text-center">
+          <h1>
+            SPLI
+            <br />
+            TTER
+          </h1>
+        </div>
+        <div className="w-full p-[3rem] pb-[4rem] bg-white text-center rounded-xl">
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+      },
+      backgroundColor: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-grayish-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "very-light-grayish-cyan": "hsl(189, 41%, 97%)",
+      }
+    },
   },
   plugins: [],
 }
